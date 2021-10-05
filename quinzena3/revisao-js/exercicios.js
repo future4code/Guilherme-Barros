e0f7ede88006c82c92eddc7dc705bd22aac74553
FIXER3600 <guilhermecomercial51@gmail.com>
2021-10-05 17:44:11 -0300

    Exercício 13

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -147,6 +147,12 @@ return filme
 // EXERCÍCIO 13
 function imprimeChamada() {
   // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+  const filme=new Object();
+filme.nome='O Diabo Veste Prada'
+filme.ano=2006
+filme.diretor='David Frankel'
+filme.atores=['Meryl Streep',' Anne Hathaway',' Emily Blunt',' Stanley Tucci']
+return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores}.`
 }
 
 // EXERCÍCIO 14
