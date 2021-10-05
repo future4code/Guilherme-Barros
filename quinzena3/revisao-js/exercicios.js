def3b4e24630698130c1b964ba273412d3f7ba32
FIXER3600 <guilhermecomercial51@gmail.com>
2021-10-05 19:35:45 -0300

    Exercício 18A

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -227,15 +227,8 @@ function verificaParidade(array) {
 
 // EXERCÍCIO 18A
 function retornaPessoasAutorizadas(pessoas) {
- /* const pessoas = [
-    { nome: "Paula", idade: 12, altura: 1.8},
-    { nome: "João", idade: 20, altura: 1.3},
-    { nome: "Pedro", idade: 15, altura: 1.9},
-    { nome: "Luciano", idade: 22, altura: 1.8},
-    { nome: "Artur", idade: 10, altura: 1.2},
-    { nome: "Soter", idade: 70, altura: 1.9}
-  ]*/
-
+const autorizadas=pessoas.filter(item=>item.altura>=1.5).filter(item=>item.idade>14 && item.idade<60)
+return autorizadas
 }
 
 // EXERCÍCIO 18B
