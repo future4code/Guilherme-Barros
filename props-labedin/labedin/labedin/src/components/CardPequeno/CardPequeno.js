f7836ae82c11b9a9467bb604c663aa1ea1665727
FIXER3600 <guilhermecomercial51@gmail.com>
2021-11-11 01:52:43 -0300

    Labedin

---
+++ props-labedin/labedin/labedin/src/components/CardPequeno/CardPequeno.js
@@ -0,0 +1,36 @@
+import React from 'react';
+//import './CardPequeno.css'
+import styled from 'styled-components'
+
+const StrongLabel = styled.strong `
+padding: 10px 5px 10px 7px;`
+
+const LittleCard=styled.div `
+display: flex;
+	align-items: center;
+	border: 1px solid black;
+	padding: 5px 5px;
+	margin-bottom: 10px;
+	height: 70px;`
+
+export default function CardPequeno(props) {
+	return(
+		<div>
+			<LittleCard>
+		<img src={ props.imagem } />
+		
+		    <StrongLabel>Email:</StrongLabel>
+		    <label>{ props.mail }</label>
+		    </LittleCard>
+		    <LittleCard>
+			<div>
+		<img src={ props.imagem } />
+		
+		    <StrongLabel>Endereço:</StrongLabel>
+		    <label>{ props.address }</label>
+		    </div>
+	    </LittleCard>
+	    </div>
+	    
+	)
+}
