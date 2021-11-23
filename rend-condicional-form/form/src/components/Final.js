81a4942cee358dcbbcd3ef2c1fad493566ea9235
FIXER3600 <guilhermecomercial51@gmail.com>
2021-11-23 01:15:50 -0300

    LabForms

---
+++ rend-condicional-form/form/src/components/Final.js
@@ -0,0 +1,15 @@
+import react from "react";
+
+
+
+export default class Final extends react.Component {
+ render(){
+  return (
+    <div className="App">
+     <h3>O FORMULÁRIO ACABOU</h3>
+     <p>Muito obrigado por participar! Entraremos em contato!</p>
+    </div>
+  );
+ }
+  
+}
