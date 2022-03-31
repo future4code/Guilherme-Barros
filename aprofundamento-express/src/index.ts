1b6d946f524ff45502b54341834327f9abd7a928
FIXER3600 <guilhermecomercial51@gmail.com>
2022-03-31 03:22:33 -0300

    aprofundamento-express

---
+++ aprofundamento-express/src/index.ts
@@ -0,0 +1,48 @@
+import express from "express";
+
+import { AddressInfo } from "net";
+
+const app = express();
+
+app.use(express.json());
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});;
+//1
+app.get("/ping", (req, res) => {          
+		res.send("Pong! 🏓")
+}
+//2
+type ToDo={
+	userId:number,
+	id:number,
+	title:string,
+	completed:boolean
+}
+//3
+const Afazeres:ToDo[]=[
+	{
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false
+  },
+]
