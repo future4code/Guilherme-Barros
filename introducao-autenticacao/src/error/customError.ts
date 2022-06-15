06ad81a0ee43e357847e967429c230049b99baf6
FIXER3600 <guilhermecomercial51@gmail.com>
2022-06-14 21:56:13 -0300

    Introducao a autenticacao

---
+++ introducao-autenticacao/src/error/customError.ts
@@ -0,0 +1,15 @@
+export class CustomError extends Error {
+	constructor(statusCode: number, message: string){
+	    super(message)
+	}
+    }
+    export class InvalidEmail extends CustomError{
+	constructor(){
+		super(400, "Email inválido")
+	    }
+    }
+    export class InvalidPassword extends CustomError{ 
+	constructor(){
+	    super(400, "Senha inválida")
+	}
+    }
